d621fcdc9033166751fd59950426f18299ab0fd1
dogusbykl <69355850+dogusbykl@users.noreply.github.com>
2026-08-13 14:07:07 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+var ExpenseApp = {
+
+    init: function () {
+
+        console.log("Expense Document Generator started.");
+
+        widget.body.innerHTML =
+            '<div class="expense-app">' +
+
+                '<h2>Expense Document Generator</h2>' +
+
+                '<p class="subtitle">' +
+                    'Manage your expense documents.' +
+                '</p>' +
+
+                '<button id="addExpenseBtn">' +
+                    'Add Expense' +
+                '</button>' +
+
+            '</div>';
+
+        document
+            .getElementById("addExpenseBtn")
+            .addEventListener("click", ExpenseApp.addExpense);
+    },
+
+
+    addExpense: function () {
+
+        alert("Add Expense clicked");
+
+    }
+
+};
+
+
+widget.addEvent("onLoad", function () {
+
+    ExpenseApp.init();
+
+});
